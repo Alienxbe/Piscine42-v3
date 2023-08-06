306539e102e99f9b7b94b3d008f30f8162b997da
Alienxbe <martin.ykman@gmail.com>
2023-08-06 19:35:54 +0200

    Adding error messages

--- rush00/ex00/main.c
+++ rush00/ex00/main.c
@@ -6,11 +6,27 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 17:24:46 by marykman          #+#    #+#             */
-/*   Updated: 2023/08/06 17:51:18 by marykman         ###   ########.fr       */
+/*   Updated: 2023/08/06 19:19:34 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	rush(int x, int y);
+
+void	print_error(char *str)
+{
+	int	i;
+
+	if (!str)
+		return ;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(2, str + i, 1);
+		i++;
+	}
+}
 
 int	ft_atoi(char *str)
 {
@@ -35,9 +51,17 @@ int	main(int argc, char **argv)
 	int	y;
 
 	if (argc != 3)
+	{
+		print_error("Error: Wrong argument count\n");
 		return (1);
+	}
 	x = ft_atoi(argv[1]);
 	y = ft_atoi(argv[2]);
+	if (x <= 0 || y <= 0)
+	{
+		print_error("Error: X and Y must be non-zero positive integers\n");
+		return (1);
+	}
 	rush(x, y);
 	return (0);
 }
